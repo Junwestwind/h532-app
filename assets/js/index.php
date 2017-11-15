603b5b870808aab944d18d97f2fd17edf7a662e1
sun <2931898310@qq.com>
2017-11-15 20:51:10 +0800

    项目首页基本完成

---
+++ assets/js/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$arr =[ 
+		["img"=>"images/carousel-area1-1.jpg","pname"=>"魅蓝 Note6","use"=>"疾速双摄 1600万前置美拍","price"=>"￥999起"],
+		["img"=>"images/carousel-area1-2.jpg","pname"=>"魅蓝 5s","use"=>"32G版限时领券立减50元","price"=>"￥1099起"],
+		["img"=>"images/carousel-area1-3.png","pname"=>"魅蓝 E8","use"=>"限时特惠100元","price"=>"￥1099起"],
+		["img"=>"images/carousel-area1-4.png","pname"=>"魅蓝 Note5","use"=>"领券最高减100元","price"=>"￥899起"],
+		["img"=>"images/carousel-area2-1.png","pname"=>"魅蓝 Note6","use"=>"疾速双摄 1600万前置美拍","price"=>"￥1099起"],
+		["img"=>"images/carousel-area2-2.png","pname"=>"魅蓝 5s","use"=>"32G版限时领券立减50元","price"=>"￥1099起"],
+		["img"=>"images/carousel-area2-3.png","pname"=>"魅蓝 E8","use"=>"限时特惠100元","price"=>"￥1099起"],
+		["img"=>"images/carousel-area2-4.png","pname"=>"魅蓝 Note5","use"=>"领券最高减100元","price"=>"￥1099起"],
+		["img"=>"images/carousel-area3-1.png","pname"=>"魅蓝 Note6","use"=>"疾速双摄 1600万前置美拍","price"=>"￥1099起"],
+		["img"=>"images/carousel-area3-2.png","pname"=>"魅蓝 5s","use"=>"32G版限时领券立减50元","price"=>"￥999起"],
+		["img"=>"images/carousel-area3-3.png","pname"=>"魅蓝 E8","use"=>"限时特惠100元","price"=>"￥1099起"],
+		["img"=>"images/carousel-area3-4.jpg","pname"=>"魅蓝 Note5","use"=>"领券最高减100元","price"=>"￥899起"]
+	];
+
+ echo json_encode($arr);
